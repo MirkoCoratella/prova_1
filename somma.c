171583ce11a0971aade46c0b88fae89f61266d76
Coratella <pippo@pippo.it>
2018-12-14 22:54:04 +0100

    Creato somma.c e modificato main.c

---
+++ somma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+	int a=0, b=0;
+	printf("Inserisci IL Primo Numero:\n");
+	scanf("%d", &a);
+	printf("Inserisci Secondo Numero\n");
+	scanf("%d", &b);
+	printf("La Somma e:\n", a+b); 
+
+	return 0;
+}
+
+
+ 
